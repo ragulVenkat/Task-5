dd4b147a81624fd0790a51a49d0ab1c51d8e1935
VenkataRamanan <113666215+ragulVenkat@users.noreply.github.com>
2024-08-27 12:41:02 +0530

    Task -5 output

---
+++ output-task-5.py
@@ -0,0 +1,12 @@
+"""
+What is the expected output of the following python code given below
+"""
+
+data = [10, 501, 22, 37, 100, 999, 87, 351]
+result = filter(lambda x: x > 4, data)
+print(list(result))
+
+
+"""
+output: [10, 501, 22, 37, 100, 999, 87, 351]
+"""
